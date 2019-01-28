00b80cde9aa09b12ab2378d5392fdfa1f38d4aab
Eduardo <edubaik@gmail.com>
2019-01-27 16:57:44 -0800

    Add album list component

--- App.js
+++ App.js
@@ -8,30 +8,25 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
+import { StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Header headerText={'Albums!'} />
+				<AlbumList />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F8F8F8',
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#F8F8F8',
+	}
 });
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
-    const { textStyle, viewStyle } = styles;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  const { headerText } = props;
 
-    return (
-        <View style={viewStyle}>
-            <Text style={textStyle}>Albums!</Text>
-        </View>
-    );
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{headerText}</Text>
+    </View>
+  );
 };
 
 const styles = {
-    viewStyle: {
-        backgroundColor: '#F8F8F8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 80,
-        paddingTop: 20,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 3 },
-        shadowOpacity: 0.2,
-        elevation: 2,
-        position: 'relative',
-    },
-    textStyle: {
-        fontSize: 20,
-    },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    paddingTop: 35,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 20,
+  },
 };
 
 export default Header;
